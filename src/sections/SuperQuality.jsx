6a0617e5badbd180b395fe530adc4c26c46dfe83
Bilal Ahmad <bilalba4oo4@gmail.com>
2024-08-04 03:47:51 -0700

    Currently working on superQuality Section

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
 
-const SuperOffer = () => {
+const SuperQuality = () => {
   return (
-    <div>SuperOffer</div>
+      <section id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+      >
+
+      </section>
   )
 }
 
-export default SuperOffer
+export default SuperQuality
